5fb818cef2b9ce95fd6ce90fcd0cd6e34cab47ac
Vadim <72280866+BagStrider@users.noreply.github.com>
2025-03-18 23:26:33 +0300

    Time zalupa (nado fixit)

---
+++ Assets/GameContent/Scripts/TimeManagment/TimeBehaviour.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class TimeBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _timeMultiplier = 1f;
+    private float _timer = 0f;
+
+    private void Awake()
+    {
+        Physics.simulationMode = SimulationMode.Script;
+    }
+
+    private void Update()
+    {
+        if (Physics.simulationMode != SimulationMode.Script)
+            return;
+
+        _timer += Time.deltaTime;
+
+        while (_timer >= Time.fixedDeltaTime)
+        {
+            _timer -= Time.fixedDeltaTime;
+            Physics.Simulate(Time.fixedDeltaTime * _timeMultiplier);
+        }
+    }
+}
