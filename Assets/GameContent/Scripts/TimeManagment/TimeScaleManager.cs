9957abf0bbb7c54618f5d08812db5d125267e041
Vadim <72280866+BagStrider@users.noreply.github.com>
2025-03-21 00:03:35 +0300

    Fix player

---
+++ Assets/GameContent/Scripts/TimeManagment/TimeScaleManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class TimeScaleManager : MonoBehaviour
+{
+    [SerializeField] private float _timeMultiplier = 1f;
+    
+    private float _fixedDeltaTimeOrig;
+    
+    private void Awake()
+    {
+        _fixedDeltaTimeOrig = Time.fixedDeltaTime;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.timeScale != 1f) return;
+            Time.timeScale = _timeMultiplier;
+            Time.fixedDeltaTime = this._fixedDeltaTimeOrig * Time.timeScale;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Time.timeScale = 1f;
+            Time.fixedDeltaTime = _fixedDeltaTimeOrig;
+        }
+    }
+}
